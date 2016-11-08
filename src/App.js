d5b62da0622bf36ac27e8d6560a8ea07dd5bfb95
keyurparalkar <keur.plkar@gmail.com>
2016-11-09 00:32:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            input:"/* add your JSX here */",
+            output:'',
+            err:''
+        }
+
+        this.update=this.update.bind(this);
+    }
+
+    update(e){
+        let code=e.target.value;
+        try{
+            this.setState({
+                output:babel.transform(code,{
+                    stage:0,
+                    loose:'all',
+                }).code,
+                err:'',
+            });
+        }
+        catch(err){
+            this.setState({err:err.message});
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <header>Error: {this.state.err}</header>
+                <div className="container">
+                    <textarea
+                       onChange={this.update}
+                       defaultValue={this.state.input}
+                    ></textarea>
+                    <div id="op">
+                    <pre >
+                       Output: {this.state.output}
+                    </pre>
+                    </div>
+                </div>
+            </div>
+            );
+    }
+}
+
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path= require ('path');
+
+var config= {
+    context: path.join(__dirname , 'src'),
+    entry:['./main.js',],
+
+    output:{
+        path: path.join( __dirname , 'www'),
+        filename: 'bundle.js',
+    },
+
+    devServer:{
+        inline:true,
+        port:3333,
+    },
+
+    module:{
+        loaders:[
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                loaders:['babel'],
+            },
+        ],
+    },
+
+    resolveLoader:{
+        root:[
+                path.join(__dirname , 'node_modules'),
+        ],
+    },
+
+    resolve:{
+        root:[ path.join( __dirname , 'node_modules')],
+    },
+};
+
+module.exports=config;
